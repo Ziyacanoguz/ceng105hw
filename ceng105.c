82f76c961ff927e56446eb5554426b8ff8270997
Ziyacanoguz <oguzziyacan@gmail.com>
2023-11-16 22:56:11 +0300

    For me

--- ceng105.c
+++ ceng105.c
@@ -2,13 +2,21 @@
 
 int main() {
     int finalGrade;
-    
+
     printf("Enter your final grade (between 0 and 100): ");
     scanf("%d", &finalGrade);
-    if (finalGrade < 45) {
-        printf("You failed the course\n");
+    if (finalGrade >= 90 && finalGrade <= 100) {
+        printf("Your grade is A\n");
+    } else if (finalGrade >= 75 && finalGrade <= 89) {
+        printf("Your grade is B\n");
+    } else if (finalGrade >= 55 && finalGrade <= 74) {
+        printf("Your grade is C\n");
+    } else if (finalGrade >= 45 && finalGrade <= 54) {
+        printf("Your grade is D\n");
+    } else if (finalGrade >= 0 && finalGrade <= 44) {
+        printf("Your grade is F\n");
     } else {
-        printf("You passed the course\n");
+        printf("Invalid grade entered. Please enter a grade between 0 and 100\n");
     }
 
     return 0;
